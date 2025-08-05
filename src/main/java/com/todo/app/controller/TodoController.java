9653c5e39c76673233718aa0b71c5e4584e010d8
井上　大 <di19970701@icloud.com>
2025-08-05 13:27:38 +0900

    バリテーション機能コメント追加再

--- src/main/java/com/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/app/controller/TodoController.java
@@ -3,16 +3,13 @@ package com.todo.app.controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.todo.app.entity.Todo;
 import com.todo.app.mapper.TodoMapper;
-
 import jakarta.validation.Valid;
 
 /**
@@ -20,77 +17,81 @@ import jakarta.validation.Valid;
  */
 @Controller
 public class TodoController {
-	
-	// DBを使えるようにするために使うもの!
-	@Autowired
-	TodoMapper todoMapper;
-	
-	// 目的のURLに対して直接アクセスするメソッド
-	@RequestMapping(value="/")
-	// controllerで作成されたデータをviewに渡す
-	public String index(Model model) {
-		// ArrayListの作成
-		ArrayList<String> List = new ArrayList<>();
-		
-		// TodoMapper.javaインタフェースのselectIncomplete( )メソッドを実行 
-		// 未完了のTodoアイテムのリストをデータベースから取得
-		 List<Todo> incompleteList = todoMapper.selectIncomplete();
-		 // 取得した未完了リストを "todos" という名前でモデルに追加.その後viewで使用
-		 model.addAttribute("todos", incompleteList);
-		 
-		// TodoMapper.javaインタフェースのselectComplete( )メソッドを実行 
-		// 完了のTodoアイテムのリストをデータベースから取得
-		 List<Todo> completeList = todoMapper.selectComplete();
-			// 取得した完了リストを "todos" という名前でモデルに追加.その後viewで使用
-		 model.addAttribute("doneTodos", completeList);
-		 
-		 // 新しいタスクを入力するためのフォームを、初期化状態で表示
-		 model.addAttribute("todo", new Todo());
-		 
-		// TodoクラスのcompareToメソッドに基づいてincompleteListをソートします
-		Collections.sort(incompleteList);
-		 
-		return "index";
-	}
-	
-	// 追加処理
-	@RequestMapping(value="/add")
-	// TodoMapper.javaインタフェースのadd( )メソッドを実行 
-	// index.htmlで入力されたデータがTodoへ入る
-	public String add(@Valid Todo todo, BindingResult bindingResult, Model model) {
-		// コメント入力todo
-		if(bindingResult.hasErrors()) {
-			List<Todo> incompleteList = todoMapper.selectIncomplete();
-			Collections.sort(incompleteList);
-			model.addAttribute("todos", incompleteList);
 
-			List<Todo> completeList = todoMapper.selectComplete();
-			// 取得した完了リストを "todos" という名前でモデルに追加.その後viewで使用
-			model.addAttribute("doneTodos", completeList);
+  // DBを使えるようにするために使うもの!
+  @Autowired
+  TodoMapper todoMapper;
 
-			return "index";
-		}
-		// エラーなければTodoアイテムをデータベースに保存する
-		todoMapper.add(todo);
-		return "redirect:/";
-	}
-	
-	// 完了済処理
-	@RequestMapping(value="/update")
-	// TodoMapper.javaインタフェースのupdate( )メソッドを実行 
-	public String update(Todo todo) {
-		// データベースのTodoアイテムを完了済とする
-		todoMapper.update(todo);
-		return "redirect:/";
-	}
-	
-	// 削除処理
-	@RequestMapping(value="/delete")
-	
-	public String delete() {
-		// TodoMapper.javaインタフェースのdelete( )メソッドを実行 
-		todoMapper.delete();
-		return "redirect:/";
-			
-	}	
+  // 目的のURLに対して直接アクセスするメソッド
+  @RequestMapping(value = "/")
+  // controllerで作成されたデータをviewに渡す
+  public String index(Model model) {
+    // ArrayListの作成
+    ArrayList<String> List = new ArrayList<>();
+
+    // TodoMapper.javaインタフェースのselectIncomplete( )メソッドを実行
+    // 未完了のTodoアイテムのリストをデータベースから取得
+    List<Todo> incompleteList = todoMapper.selectIncomplete();
+    // 取得した未完了リストを "todos" という名前でモデルに追加.その後viewで使用
+    model.addAttribute("todos", incompleteList);
+
+    // TodoMapper.javaインタフェースのselectComplete( )メソッドを実行
+    // 完了のTodoアイテムのリストをデータベースから取得
+    List<Todo> completeList = todoMapper.selectComplete();
+    // 取得した完了リストを "todos" という名前でモデルに追加.その後viewで使用
+    model.addAttribute("doneTodos", completeList);
+
+    // 新しいタスクを入力するためのフォームを、初期化状態で表示
+    model.addAttribute("todo", new Todo());
+
+    // TodoクラスのcompareToメソッドに基づいてincompleteListをソートします
+    Collections.sort(incompleteList);
+
+    return "index";
+  }
+
+  // 追加処理
+  @RequestMapping(value = "/add")
+  // TodoMapper.javaインタフェースのadd( )メソッドを実行
+  // index.htmlで入力されたデータがTodoへ入る
+  public String add(@Valid Todo todo, BindingResult bindingResult, Model model) {
+    // バリデーション機能でエラーがあった際
+    if (bindingResult.hasErrors()) {
+      // データベースから未完了リストのtodoリストを取得して(incompleteList)に格納される
+      List<Todo> incompleteList = todoMapper.selectIncomplete();
+      // 未完了リストをソートする
+      Collections.sort(incompleteList);
+      // 未完了リストを"todos"という名前でviewへ渡す
+      model.addAttribute("todos", incompleteList);
+
+      List<Todo> completeList = todoMapper.selectComplete();
+      // 取得した完了リストを "todos" という名前でモデルに追加.その後viewで使用
+      model.addAttribute("doneTodos", completeList);
+
+      return "index";
+    }
+    // エラーなければTodoアイテムをデータベースに保存する
+    todoMapper.add(todo);
+    return "redirect:/";
+  }
+
+
+  // 完了済処理
+  @RequestMapping(value = "/update")
+  // TodoMapper.javaインタフェースのupdate( )メソッドを実行
+  public String update(Todo todo) {
+    // データベースのTodoアイテムを完了済とする
+    todoMapper.update(todo);
+    return "redirect:/";
+  }
+
+  // 削除処理
+  @RequestMapping(value = "/delete")
+
+  public String delete() {
+    // TodoMapper.javaインタフェースのdelete( )メソッドを実行
+    todoMapper.delete();
+    return "redirect:/";
+
+  }
 }
--- src/main/java/com/todo/app/entity/Todo.java
+++ src/main/java/com/todo/app/entity/Todo.java
@@ -1,9 +1,7 @@
 package com.todo.app.entity;
 
 import java.time.LocalDate;
-
 import org.springframework.format.annotation.DateTimeFormat;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -12,67 +10,66 @@ import lombok.Data;
 @Data
 
 /**
- * todoアイテムを表すエンティティクラス
- * DBのテーブルに値する
+ * todoアイテムを表すエンティティクラス DBのテーブルに値する
  */
 
-public class Todo implements Comparable<Todo> { 
+public class Todo implements Comparable<Todo> {
 
-	private long id;
-	@NotBlank(message = "空白ではだめです")
-	private String title;
+  private long id;
+  @NotBlank(message = "空白ではだめです")
+  private String title;
 
-	// 時間や日付のフォーマットの指定
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@NotNull(message = "日付を入れてください")
-	private LocalDate time_limit;
+  // 時間や日付のフォーマットの指定
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  @NotNull(message = "日付を入れてください")
+  private LocalDate time_limit;
 
-	private int done_flg;
+  private int done_flg;
 
-	// このクラスではあくまでもソートのルールを定義している
-	@Override
-	public int compareTo(Todo otherTodo) {
-		// time_limit(期限日時で昇順ソート)
-		// compareToメソッドの戻り値ルール
-		// 負の整数を返す　thisオブジェクトがotherTodoオブジェクトよりも「小さい」(ソート順で前に来る)
-		// ゼロを返す thisオブジェクトがotherTodoオブジェクトが等しい場合
-		// 正の整数を返す thisオブジェクトがotherTodoオブジェクトよりも大きい
+  // このクラスではあくまでもソートのルールを定義している
+  @Override
+  public int compareTo(Todo otherTodo) {
+    // time_limit(期限日時で昇順ソート)
+    // compareToメソッドの戻り値ルール
+    // 負の整数を返す thisオブジェクトがotherTodoオブジェクトよりも「小さい」(ソート順で前に来る)
+    // ゼロを返す thisオブジェクトがotherTodoオブジェクトが等しい場合
+    // 正の整数を返す thisオブジェクトがotherTodoオブジェクトよりも大きい
 
-		// time_limitがnullの場合（期限が未設定の場合）の扱いを特別に考慮
-		// 「期限が設定されているタスクを大きい、未設定のタスクを小さい」というロジックにする
+    // time_limitがnullの場合（期限が未設定の場合）の扱いを特別に考慮
+    // 「期限が設定されているタスクを大きい、未設定のタスクを小さい」というロジックにする
 
-		// 1両方がnull場合(どちらともタスク期限が設定されていない場合)
-		if(time_limit == null && otherTodo.time_limit == null) {
-			// 両方がnull場合IDで比較
-			return Long.compare(this.id, otherTodo.id);
+    // 1両方がnull場合(どちらともタスク期限が設定されていない場合)
+    if (time_limit == null && otherTodo.time_limit == null) {
+      // 両方がnull場合IDで比較
+      return Long.compare(this.id, otherTodo.id);
 
-			// 2現タスクがthis.time_limitがnullで比較対象のタスク (otherTodo) には期限が設定されている
-		} else if(this.time_limit == null) {
-			// thisがnull期限なしなので、otherTodo(期限あり)よりも「大きい」(ソート順で後)と判断
-			// これにより、期限が設定されていないタスクは、期限が設定されているタスクの後に並ぶ
-			return 1;
+      // 2現タスクがthis.time_limitがnullで比較対象のタスク (otherTodo) には期限が設定されている
+    } else if (this.time_limit == null) {
+      // thisがnull期限なしなので、otherTodo(期限あり)よりも「大きい」(ソート順で後)と判断
+      // これにより、期限が設定されていないタスクは、期限が設定されているタスクの後に並ぶ
+      return 1;
 
-			// 3現在のタスク (this) には期限が設定されており、比較対象のタスク (otherTodo) の time_limit が null の場合
-		} else if(otherTodo.time_limit == null) {
-			// otherTodoがnull（期限なし）なので、this（期限あり）よりも「小さい」（ソート順で前）と判断
-			// これにより、期限が設定されているタスクは、期限が設定されていないタスクの前に並ぶ
-			return -1;
+      // 3現在のタスク (this) には期限が設定されており、比較対象のタスク (otherTodo) の time_limit が null の場合
+    } else if (otherTodo.time_limit == null) {
+      // otherTodoがnull（期限なし）なので、this（期限あり）よりも「小さい」（ソート順で前）と判断
+      // これにより、期限が設定されているタスクは、期限が設定されていないタスクの前に並ぶ
+      return -1;
 
-			// 4両方のタスクのtime_limit が null でない場合（どちらのタスクも期限が設定されている）
-		} else {
-			// java.util.Date クラスの compareTo メソッドを使用して、日付を直接比較
-			// この比較結果は、thisの日付がotherTodoの日付より「過去」なら負、「同じ」なら0、「未来」なら正の値を返す
-			int dateComparison = this.time_limit.compareTo(otherTodo.time_limit);
-			// 日付が異なる場合（比較結果が0でない場合）
-			if(dateComparison != 0) {
-				// その比較結果（日付の昇順）を最終的なソート順として返す
-				return dateComparison;
-			} else {
-				// long型のidを比較し、その結果を返します。
-				// Long.compare(a, b) は、aがbより小さい場合は負、等しい場合は0、大きい場合は正を返します。
-				// これにより、idが小さいものがソート順で前に来る（昇順）となります。
-				return Long.compare(this.id,otherTodo.id);
-			}
-		}
-	}
+      // 4両方のタスクのtime_limit が null でない場合（どちらのタスクも期限が設定されている）
+    } else {
+      // java.util.Date クラスの compareTo メソッドを使用して、日付を直接比較
+      // この比較結果は、thisの日付がotherTodoの日付より「過去」なら負、「同じ」なら0、「未来」なら正の値を返す
+      int dateComparison = this.time_limit.compareTo(otherTodo.time_limit);
+      // 日付が異なる場合（比較結果が0でない場合）
+      if (dateComparison != 0) {
+        // その比較結果（日付の昇順）を最終的なソート順として返す
+        return dateComparison;
+      } else {
+        // long型のidを比較し、その結果を返します。
+        // Long.compare(a, b) は、aがbより小さい場合は負、等しい場合は0、大きい場合は正を返します。
+        // これにより、idが小さいものがソート順で前に来る（昇順）となります。
+        return Long.compare(this.id, otherTodo.id);
+      }
+    }
+  }
 }
